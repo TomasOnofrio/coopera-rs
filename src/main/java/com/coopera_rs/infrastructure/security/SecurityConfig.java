3587eec3d909515f4356382ce951d080aa33fff4
Joao Vitor M <joao.vitorms1509@gmail.com>
2025-03-29 02:05:20 -0300

    hotfix: disable frame and endpoints block

--- src/main/java/com/coopera_rs/infrastructure/security/SecurityConfig.java
+++ src/main/java/com/coopera_rs/infrastructure/security/SecurityConfig.java
@@ -1,21 +1,34 @@
 package com.coopera_rs.infrastructure.security;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.security.Key;
+import java.util.Base64;
 
 @Configuration
 public class SecurityConfig {
-    
+
+    @Value("${spring.security.oauth2.resourceserver.jwt.secret}")
+    private String secret;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         http.csrf(csrf -> csrf.disable())
+            .headers(headers -> headers.frameOptions(frameOptions -> frameOptions.disable()))
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/public/**").permitAll()
+                .requestMatchers("/api/auth/**").permitAll()
+                .requestMatchers("/h2-console/**").permitAll()
                 .anyRequest().authenticated()
             )
             .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
@@ -25,5 +38,15 @@ public class SecurityConfig {
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration)throws Exception{
         return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        byte[] decodedKey = Base64.getDecoder().decode(secret);
+        Key key = new SecretKeySpec(decodedKey, "HmacSHA256");
+
+        return NimbusJwtDecoder
+                .withSecretKey((SecretKey) key)
+                .build();
     }
 }
