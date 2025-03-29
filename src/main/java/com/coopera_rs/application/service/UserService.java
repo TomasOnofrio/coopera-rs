076a77a55814f6ef131642161b9bc6d5c6d908ef
Joao Vitor M <joao.vitorms1509@gmail.com>
2025-03-29 01:08:46 -0300

    feat: Add UserService

---
+++ src/main/java/com/coopera_rs/application/service/UserService.java
@@ -0,0 +1,47 @@
+package com.coopera_rs.application.service;
+
+import java.lang.StackWalker.Option;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import com.coopera_rs.infrastructure.security.PasswordEncoderConfig;
+
+import jakarta.validation.Valid;
+
+import org.springframework.stereotype.Service;
+
+import com.coopera_rs.core.User;
+import com.coopera_rs.core.port.UserRepository;
+
+@Service
+public class UserService {
+    
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserService(UserRepository userRepository, PasswordEncoderConfig passwordEncoderConfig) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoderConfig.passwordEncoder();
+    }
+
+    public User registerUser(@Valid User user){
+        Optional<User> existingUser = userRepository.findByEmail(user.getEmail());
+        if (existingUser.isPresent()) {
+            throw new IllegalArgumentException("Email atualmente em uso");
+        }
+
+        Optional<User> existingUserByUsername = userRepository.findByUsername(user.getUsername());
+        if (existingUserByUsername.isPresent()) {
+            throw new IllegalArgumentException("Nome de usuário já existe");
+        }
+
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+
+        return userRepository.save(user);
+    }
+
+    public List<User> listAllUsers(){
+        return userRepository.findAll();
+    }
+}
